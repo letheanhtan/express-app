b1e48af9df79a08b15afbd22eda45131a822e7f7
letheanhtan <letheanhtan@gmail.com>
2020-06-12 15:21:10 +0700

    add User model and User record

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+    name: String,
+    email: String
+})
+
+const User = mongoose.model("User", UserSchema)
+module.exports = User
+
+const user_resource = new User({
+    name: 'John Doe',
+    email: 'john@doe.com'
+})
+
+user_resource.save((error) => {
+    if(error)
+        console.log(error)
+    
+    res.send({
+        success: true,
+        code: 200,
+        msg: "User add!"
+    })
+})
